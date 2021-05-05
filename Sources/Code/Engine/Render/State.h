f92f0da9804150e72729303723062bfc20e5c683
SteaveP <Swoo2n@gmail.com>
2021-05-05 23:25:37 +0300

    WIP: Vulkan

---
+++ Sources/Code/Engine/Render/State.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "Engine/EngineModule.h"
+
+namespace Cyclone::Render
+{
+
+enum class DepthStencilState
+{
+};
+
+enum class RasterizerState
+{
+
+};
+
+} // namespace Cyclone::Render
