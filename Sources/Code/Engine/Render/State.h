e2451abcad73ddef54425e64656af90007a80e68
SteaveP <Swoo2n@gmail.com>
2021-05-11 17:21:02 +0300

    WIP: Render

--- Sources/Code/Engine/Render/State.h
+++ Sources/Code/Engine/Render/State.h
@@ -7,11 +7,47 @@ namespace Cyclone::Render
 
 enum class DepthStencilState
 {
+
 };
 
 enum class RasterizerState
 {
 
+};
+
+enum class BlendState
+{
+
+};
+
+enum class PixelType
+{
+    RGBA8,
+    RGBA8_SRGB,
+    RG8,
+    R8,
+    R10G10B10A2,
+    R11G11B10,
+    RGBA16,
+    RG16,
+    R16,
+    RGBA32,
+    RGB32,
+    RG32,
+    R32,
+    BC1,
+    BC2,
+    BC3,
+    BC4,
+    BC5,
+    BC6,
+    BC7,
+    // Depth
+    D_16,
+    D_24,
+    D_32,
+    //
+    Count
 };
 
 } // namespace Cyclone::Render
