d28e860da23dd784dad76eb8dd9b1e7e1a8e7548
SteaveP <Swoo2n@gmail.com>
2021-05-12 23:59:40 +0300

    WIP: Render

---
+++ Sources/Code/Engine/Render/CommandBuffer.cpp
@@ -0,0 +1,6 @@
+#include "CommandBuffer.h"
+
+namespace Cyclone::Render
+{
+
+} // namespace Cyclone::Render
---
+++ Sources/Code/Engine/Render/CommandBuffer.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "Engine/EngineModule.h"
+#include "Engine/Framework/IRenderer.h"
+
+namespace Cyclone::Render
+{
+
+class CommandBuffer
+{
+public:
+    // Set State commands
+    // Draw commands
+    // Dispatch commands
+    // Copy commands
+
+protected:
+    // backend command buffer
+};
+
+} // namespace Cyclone::Render
